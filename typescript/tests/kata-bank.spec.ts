715b77e53fbf1bfd591c4717641a6650ea68b53e
Carlos Jaramillo <cejaramillof@gmail.com>
2023-03-31 11:56:07 -0500

    Add acceptance test

---
+++ typescript/tests/kata-bank.spec.ts
@@ -0,0 +1,28 @@
+
+class Account {
+    printStatement() {
+        throw new Error("Method not implemented.");
+    }
+    withdraw(arg0: number) {
+        throw new Error("Method not implemented.");
+    }
+    deposit(arg0: number) {
+        throw new Error("Method not implemented.");
+    }
+  // Implement
+}
+
+describe.skip(`Given a client makes a deposit of 1000 on 20/01/2022
+        And a deposit of 2000 on 23/01/2022
+        And a withdrawal of 500 on 24/01/2022`, () => {
+    it('When she prints her bank statement', () => {
+        jest.spyOn(global.console, 'log')
+        const account = new Account();
+        account.deposit(1000);
+        account.deposit(2000);
+        account.withdraw(500);
+        account.printStatement();
+        // Then she would see at the terminal
+        expect(console.log).toBeCalledWith();
+    });
+});
